441598414086976f7f8d65f7c6e871109db9a431
O.R <orezek@seznam.cz>
2023-12-02 16:35:39 +0100

    Update: add include statements confirm to the project layout

--- minishell.h
+++ minishell.h
@@ -6,12 +6,12 @@
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 16:24:52 by aldokezer         #+#    #+#             */
-/*   Updated: 2023/12/02 16:26:24 by aldokezer        ###   ########.fr       */
+/*   Updated: 2023/12/02 16:31:41 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "unistd.h"
-
 #ifndef MINISHELL_H
 # define MINISHELL_H
+# include "unistd.h"
+# include "./libft/libft.h"
 #endif
