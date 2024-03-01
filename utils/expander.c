51e782aebfa4d32ea400ddb238b0a5e02e192600
m-bartos <m.bartos96@seznam.cz>
2024-03-01 17:12:21 +0100

    Fix: add more ending characters of variable

--- utils/expander.c
+++ utils/expander.c
@@ -6,43 +6,53 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 10:33:30 by mbartos           #+#    #+#             */
-/*   Updated: 2024/03/01 13:27:11 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/03/01 17:11:06 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include <stdlib.h>
 #include <stdio.h>
 #include "../libft/libft.h"
 
+char	*end_of_variable(char *str)
+{
+	while (*str != '$' && *str != '\0' && *str != '/' && *str != ':'
+		&& *str != ';' && *str != '-' && *str != '%')
+		str++;
+	return (str);
+}
 
 char	*get_variable_name(char	*str)
 {
 	size_t	size_to_malloc;
 	size_t	index;
-	char	*str_out;
+	char	*str_variable;
+	char	*str_out;	
 
-	size_to_malloc = 0;
-	while (str[size_to_malloc] != '\0' && str[size_to_malloc] != '/')
-		size_to_malloc++;
+	str_variable = ft_strchr(str, '$') + 1;
+	size_to_malloc = end_of_variable(str_variable) - str_variable;
 	str_out = (char *) malloc(sizeof(char) * (size_to_malloc + 1));
 	if (!str_out)
 		return (NULL);
 	index = 0;
 	while (index < size_to_malloc)
 	{
-		str_out[index] = str[index];
+		str_out[index] = str_variable[index];
 		index++;
 	}
 	str_out[index] = '\0';
 	return (str_out);
 }
 
-char	*end_of_variable(char *str)
+char	*find_and_expand_var(char *str)
 {
-	while (*str != '\0' && *str != '/')
-		str++;
-	return (str);
+	char	*str_variable;
+	char	*str_extended_variable;
+
+	str_variable = get_variable_name(str);
+	str_extended_variable = getenv(str_variable);
+	free(str_variable);
+	return (str_extended_variable);
 }
 
 char	*expander(char *str)
@@ -61,18 +71,13 @@ char	*expander(char *str)
 	// find it in getenv
 	// compute the size to allocate new string
 	// malloc
-	// 
-
 	while (ft_strchr(str, '$'))
 	{
-		str_variable = ft_strchr(str, '$');
-		str_variable = get_variable_name(++str_variable);
-		str_expanded_variable = getenv(str_variable);
-		free(str_variable);
+		str_expanded_variable = find_and_expand_var(str);
 		size_to_malloc = ft_strchr(str, '$') - str;
 		if (str_expanded_variable != NULL)
 			size_to_malloc = size_to_malloc + ft_strlen(str_expanded_variable);
-		size_to_malloc = size_to_malloc + ft_strchr(str, '\0') - end_of_variable(ft_strchr(str, '$'));
+		size_to_malloc = size_to_malloc + ft_strchr(str, '\0') - end_of_variable(ft_strchr(str, '$') + 1);
 		str_out = (char *) malloc(sizeof(char) * (size_to_malloc + 1));
 		if (!str_out)
 			return (NULL);
@@ -88,7 +93,7 @@ char	*expander(char *str)
 			while (str_expanded_variable[j] != '\0')
 				str_out[index++] = str_expanded_variable[j++];
 		}
-		str_rest = end_of_variable(ft_strchr(str, '$'));
+		str_rest = end_of_variable(ft_strchr(str, '$') + 1);
 		j = 0;
 		while (str_rest[j] != '\0')
 			str_out[index++] = str_rest[j++];
@@ -100,18 +105,14 @@ char	*expander(char *str)
 	return (str);
 }
 
-// cc expander.c ../libft/ft_strchr.c ../libft/ft_strlen.c ../libft/ft_strdup.c 
-
-/*
 int	main (void)
 {
-	char	str[] = "aha$PATH/a\n123/$PAT123H/B";
+	char	str[] = "aha$PATH/a\naa123/$PATH$PATH-/B";
 	char	*str_dynamic;
 
-	str_dynamic = ft_strdup(str);	
+	str_dynamic = ft_strdup(str);
 	printf("INPUT:\n%s\n\n", str_dynamic);
 	str_dynamic = expander(str_dynamic);
 	printf("OUTPUT:\n%s\n", str_dynamic);
 	free(str_dynamic);
 }
-*/
