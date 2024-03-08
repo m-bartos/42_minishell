06f1c8a5a41cdc3f1cfc2acdd8abeda627897da6
Oldrich Rezek <orezek@seznam.cz>
2024-03-08 16:52:58 +0100

    Remove: remove further code to start clean

--- main_exe.c
+++ main_exe.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main_exe.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
+/*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 12:36:32 by aldokezer         #+#    #+#             */
-/*   Updated: 2024/03/06 16:31:23 by aldokezer        ###   ########.fr       */
+/*   Updated: 2024/03/08 16:49:33 by orezek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,11 @@
 
 int	main(void)
 {
-	t_cmd_tab cmd_table;
-	char	**arr;
 
-	ft_init_command_table(&cmd_table);
-	char	*command = "ls -la | wc -l";
-	arr = ft_split(command, ' ');
-	while (*arr)
-	{
-		ft_push_rotate_token(&cmd_table, *arr);
-		arr++;
-	}
-
-	char *cmd8[] = {"<", NULL};
-	char *cmd9[] = {"infile.txt", NULL};
-	char *cmd0[] = {"/bin/cat", NULL};
 	char *cmd1[] = {"/bin/ls", "-la", NULL};
 	char *cmd2[] = {"/usr/bin/wc", "-l", NULL};
-	char *cmd3[] = {"/usr/bin/wc", "-c", NULL};
-	char *cmd4[] = {"/bin/cat", NULL};
-	char **cmds[] = {cmd8, cmd9, cmd0, cmd1, cmd2, cmd3, cmd4, NULL};
-	//char **cmds[] = {cmd8, cmd9, cmd0, NULL};
+
+	char **cmds[] = {cmd1, cmd2, NULL};
 
 	ft_exec_commands(cmds);
 
--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 16:24:52 by aldokezer         #+#    #+#             */
-/*   Updated: 2024/03/08 16:26:00 by orezek           ###   ########.fr       */
+/*   Updated: 2024/03/08 16:52:29 by orezek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,7 @@
 # include "./libft/libft.h"
 # include <sys/stat.h>
 # include <fcntl.h>
-// rl_clear_history, rl_on_new_line,
-// rl_replace_line, rl_redisplay, add_history,
-# include <readline/readline.h>
-// adds rl_clear_history()
-# include <readline/history.h>
-
-# define RESET   "\x1B[0m"
-# define BLUE    "\x1B[34m"
+# include <sys/wait.h>
 
 # define STDIN 0
 # define STDOUT 1
@@ -35,37 +28,6 @@
 # define FALSE 0
 # define TRUE 1
 
-// typedef enum s_type
-// {
-// 	CMD,
-// 	CMD_ERR,
-// 	ARG,
-// 	PIPE,
-// 	R_IN,
-// 	R_OUT,
-// 	R_OUT_APPND,
-// 	HERE_DOC
-// }		t_type;
-
-// // chaning declaration of *next from t_node to struct s_node due to compilation error on Mac
-// typedef struct s_node
-// {
-// 	char			*token;
-// 	t_type			type;
-// 	struct s_node	*next;
-// 	struct s_node	*prev;
-// }		t_node;
-
-// /* --- LINKED LIST EXAMPLE (non-sense command) ---
-// echo	/	aaa 	/	bbb		/	|		/	<		/	file.txt	/	cat			/
-// CMD	/	ARG		/	ARG		/	PIPE	/	R_IN	/	ARG			/	CMD		/
-// */
-// typedef struct s_command_table
-// {
-// 	t_node	*first_node;
-// 	t_node	*last_node;
-// 	int		size;
-// }		t_cmd_tab;
 
 // new types to categorize tokens
 typedef enum s_type
@@ -89,18 +51,18 @@ typedef struct s_token
 {
 	char			*text;
 	t_type			type;
-	t_token			*next;
-	t_token			*prev;
+	struct s_token	*next;
+	struct s_token	*prev;
 }		t_token;
 
 typedef struct s_command
 {
-	char		**execve_cmd;
-	t_command	*next_cmd;
-	t_command	*prev_cmd;
-	t_token		*first_token;
-	t_token		*last_token;
-	int			size;
+	char				**execve_cmd;
+	struct s_command	*next_cmd;
+	struct s_command	*prev_cmd;
+	t_token				*first_token;
+	t_token				*last_token;
+	int					size;
 }		t_command;
 
 typedef struct s_cmd_tab
@@ -111,7 +73,6 @@ typedef struct s_cmd_tab
 }		t_cmd_tab;
 
 
-
 // command_table functions
 void	ft_init_command_table(t_cmd_tab *cmd_table);
 void	ft_rotate_token(t_cmd_tab *cmd_table);
@@ -119,67 +80,9 @@ void	ft_push_token(t_cmd_tab *cmd_table, char *token);
 void	ft_push_rotate_token(t_cmd_tab *cmd_table, char *token);
 void	ft_delete_nodes(t_cmd_tab *cmd_table);
 
-// expander_var.c
-char	*end_of_var(char *str);
-char	*get_variable_name(char	*str);
-char	*get_expanded_var(char *str);
-
-// expander.c
-// char	*expand_all_vars_in_str(char *str);
-void	expand_cmd_tab(t_cmd_tab *ptr_cmd_tab);
-
-// get_prompt.c
-char	*get_prompt(void);
-
-// parser.c
-void	parser(t_cmd_tab *ptr_cmd_tab, char *line);
-
-// splitter.c
-char	**splitter(char *line);
-
-// splitter_handlers.c
-char	*handle_redirections(char *str, size_t *index, char redir_type);
-char	*handle_pipe(char *str, size_t *index);
-char	*handle_quotes(char *str, size_t *index, char quotes_type);
-char	*handle_word(char *str, size_t *index);
-
-// splitter_utils.c
-int		is_whitespace(char c);
-int		is_quote(char c);
-int		is_operator(char c);
-int		is_end_of_word(char c);
-size_t	word_length(char *str);
-
-// cmd_tab_assign_types.c
-void	assign_types_to_tokens(t_cmd_tab *ptr_cmd_tab);
-
-
-// cmd_tab_filler.c
-void	init_and_fill_cmd_tab(t_cmd_tab *ptr_cmd_tab, char **arr_of_tokens);
-void	print_cmd_tab(t_cmd_tab *ptr_cmd_tab);
-
-// cmd_tab_filler_utils.c
-int		is_pipe_type(t_node *ptr_node);
-int		is_redirection_type(t_node *ptr_node);
-int		is_operator_type(t_node *ptr_node);
-int		is_in_single_quotes(t_node *ptr_node);
-int		is_in_double_quotes(t_node *ptr_node);
-
-// cmd_tab_remove_quotes.c
-void	remove_quotes_from_cmd_tab(t_cmd_tab *ptr_cmd_tab);
-
-void	ft_init_command_table(t_cmd_tab *cmd_table);
-void	ft_rotate_token(t_cmd_tab *cmd_table);
-void	ft_push_token(t_cmd_tab *cmd_table, char *token);
-void	ft_push_rotate_token(t_cmd_tab *cmd_table, char *token);
-void	ft_delete_nodes(t_cmd_tab *cmd_table);
-
-// helper functions
-void	ft_print_cmd(t_cmd_tab *cmd_table);
-
 // exec functions
 void	ft_exec_commands(char ***cmds);
 
-// input output
+// input output functions
 int		ft_input_file(char *file_name);
 #endif
