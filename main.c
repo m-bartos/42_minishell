16f2f28856dc2d359844d9e4569e8c6ebec901f2
m-bartos <m.bartos96@seznam.cz>
2024-03-08 16:34:19 +0100

    Update: main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 14:09:57 by aldokezer         #+#    #+#             */
-/*   Updated: 2024/03/08 16:29:10 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/03/08 16:33:33 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,12 +56,14 @@ void	check_unclosed_quotes(t_cmd *cmd, char *line, char *prompt)
 // echo "jojo" 'nene' > outfile.txt | < infile.txt echo "jojo" 'nene' aha "$USER" '$USER' $USER >> test.out
 int	main (void)
 {
-	char	*line;
-	char	*prompt;
-	t_cmd	cmd;
+	char		*line;
+	char		*prompt;
+	t_cmd		parsed_line;
+	// t_cmd_tab	cmd_tab;
 
 	line = NULL;
-	ft_init_cmd_struct(&cmd);
+	ft_init_cmd_struct(&parsed_line);
+	// ft_init_cmd_tab_struct(&cmd_tab);
 	while (1)
 	{
 		printf(BLUE);
@@ -69,13 +71,14 @@ int	main (void)
 		line = readline(prompt);
 		if (*line)
 			add_history(line);
-		check_exit(&cmd, line, prompt);
-		check_unclosed_quotes(&cmd, line, prompt);
-		parser(&cmd, line);
-		check_double_redirect(&cmd, line, prompt);
-		handle_if_last_is_pipe(&cmd);
-		print_cmd_tab(&cmd); // just show table
-		free_program(&cmd, line, prompt);
+		check_exit(&parsed_line, line, prompt);
+		check_unclosed_quotes(&parsed_line, line, prompt);
+		parser(&parsed_line, line);
+		check_double_redirect(&parsed_line, line, prompt);
+		handle_if_last_is_pipe(&parsed_line);
+		print_cmd_tab(&parsed_line); // just show table
+		// make_cmd_tab(&parsed_line, &cmd_tab);
+		free_program(&parsed_line, line, prompt);
 	}
 	return (0);
 }
