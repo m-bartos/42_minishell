91324921207c5d37a6f87bfd2c82eff57cd3ee7e
m-bartos <m.bartos96@seznam.cz>
2024-03-10 10:21:01 +0100

    Update: here_doc with var expansion

--- utils/here_doc.c
+++ utils/here_doc.c
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 10:17:25 by mbartos           #+#    #+#             */
-/*   Updated: 2024/03/10 10:12:31 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/03/10 10:14:00 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,7 @@ char	*get_heredoc_file(char *eof, int index)
 		old_line = line;
 		line = ft_strjoin(line, "\n");
 		free(old_line);
+		line = expand_all_vars_in_str(line);
 		write(fd, line, ft_strlen(line));
 		free(line);
 	}
