4d81413540b422ab33cdd02639c1f13da95fa8c6
O.R <orezek@seznam.cz>
2024-03-10 15:03:16 +0100

    Fix: error for output_file()

--- utils/input_output.c
+++ utils/input_output.c
@@ -6,7 +6,7 @@
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 22:05:51 by aldokezer         #+#    #+#             */
-/*   Updated: 2024/03/09 00:51:14 by aldokezer        ###   ########.fr       */
+/*   Updated: 2024/03/10 15:00:39 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int	ft_output_file(char *file_name)
 {
 	int	fd;
 
-	fd = open(file_name, O_WRONLY | O_CREAT, S_IRWXU | S_IRGRP | S_IROTH);
+	fd = open(file_name, O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU | S_IRGRP | S_IROTH);
 	if (fd == -1)
 		return (-1);
 	else
