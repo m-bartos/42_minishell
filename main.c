8b4c38756245f70414be9538a91f11b72a3dd799
m-bartos <m.bartos96@seznam.cz>
2024-03-11 12:07:27 +0100

    Update: main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 14:09:57 by aldokezer         #+#    #+#             */
-/*   Updated: 2024/03/11 12:05:21 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/03/11 12:07:20 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@ void	handle_if_last_is_pipe(t_cmd *cmd)
 	while (is_pipe_type(cmd->last_token))
 	{
 		line_heredoc = readline("> ");
-		// check_unclosed_quotes(line_heredoc); // need to check it and if there are unclosed quotes, free whole cmd
 		arr_of_tokens = splitter(line_heredoc);
 		free(line_heredoc);
 		parser(cmd, arr_of_tokens);
@@ -54,14 +53,14 @@ int	main (void)
 		free(line);
 		parser(&parsed_line, arr_of_tokens);
 		handle_if_last_is_pipe(&parsed_line);
-		// print_cmd(&parsed_line); // just show table
+		// print_cmd(&parsed_line);
 		make_cmd_tab(&cmd_tab, &parsed_line);
 		ft_delete_cmd(&parsed_line);
 		// print_cmd_tab(&cmd_tab);
 		// print_cmd_tab(&cmd_tab);
 		make_cmd_paths(&cmd_tab);
 		make_execve_cmds(&cmd_tab);
-		print_cmd_tab(&cmd_tab);
+		print_cmd_tab(&cmd_tab); // just to show cmd_tab
 		ft_delete_cmds_in_cmd_tab(&cmd_tab);
 	}
 	return (0);
