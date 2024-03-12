c8263fa21f0f92638d93a180e0808b988ea7f5d5
m-bartos <m.bartos96@seznam.cz>
2024-03-12 12:58:49 +0100

    Clean: input_output.c

--- utils/input_output.c
+++ utils/input_output.c
@@ -3,20 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   input_output.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
+/*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 22:05:51 by aldokezer         #+#    #+#             */
-/*   Updated: 2024/03/05 22:20:12 by aldokezer        ###   ########.fr       */
+/*   Updated: 2024/03/12 12:58:36 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-int	ft_input_file(char *file_name)
+int	ft_input_file(char *filename)
 {
 	int	fd;
 
-	fd = open(file_name, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (-1);
 	else
