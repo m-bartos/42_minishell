1049de050f5d4c40a1e7aa4dd5f8e7053753783f
m-bartos <m.bartos96@seznam.cz>
2024-03-12 11:47:26 +0100

    Clean: cmd_struct_assign_types.c

--- utils/cmd_struct_assign_types.c
+++ utils/cmd_struct_assign_types.c
@@ -6,42 +6,42 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 14:37:34 by mbartos           #+#    #+#             */
-/*   Updated: 2024/03/12 11:26:03 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/03/12 11:44:08 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void	assign_operator_types(t_cmd *ptr_cmd)
+void	assign_operator_types(t_cmd *cmd)
 {
-	t_token	*ptr_node;
+	t_token	*token;
 
-	ptr_node = ptr_cmd->first_token;
-	while (ptr_node != NULL)
+	token = cmd->first_token;
+	while (token != NULL)
 	{
-		if (!ft_strncmp(ptr_node->text, "|", 2))
-			ptr_node->type = PIPE;
-		else if (!ft_strncmp(ptr_node->text, "<<", 3))
-			ptr_node->type = HERE_DOC;
-		else if (!ft_strncmp(ptr_node->text, "<", 2))
-			ptr_node->type = R_IN;
-		else if (!ft_strncmp(ptr_node->text, ">>", 3))
-			ptr_node->type = R_OUT_APP;
-		else if (!ft_strncmp(ptr_node->text, ">", 2))
-			ptr_node->type = R_OUT;
-		ptr_node = ptr_node->next;
+		if (ft_strncmp(token->text, "|", 2) == 0)
+			token->type = PIPE;
+		else if (ft_strncmp(token->text, "<<", 3) == 0)
+			token->type = HERE_DOC;
+		else if (ft_strncmp(token->text, "<", 2) == 0)
+			token->type = R_IN;
+		else if (ft_strncmp(token->text, ">>", 3) == 0)
+			token->type = R_OUT_APP;
+		else if (ft_strncmp(token->text, ">", 2) == 0)
+			token->type = R_OUT;
+		token = token->next;
 	}
 }
 
-int	assign_file_type(int type)
+int	assign_file_type(int prev_token_type)
 {
-	if (type == R_IN)
+	if (prev_token_type == R_IN)
 		return (R_INFILE);
-	else if (type == R_OUT)
+	else if (prev_token_type == R_OUT)
 		return (R_OUTFILE);
-	else if (type == R_OUT_APP)
+	else if (prev_token_type == R_OUT_APP)
 		return (R_OUTFILE_APP);
-	else if (type == HERE_DOC)
+	else if (prev_token_type == HERE_DOC)
 		return (HERE_DOC_EOF);
 	return (99);
 }
@@ -64,39 +64,39 @@ int	assign_cmd_type(char *text)
 		return (CMD);
 }
 
-void	assign_cmds_and_args(t_cmd *ptr_cmd)
+void	assign_cmds_and_args(t_cmd *cmd)
 {
-	t_token	*ptr_node;
+	t_token	*token;
 	int		search_cmd;
-	int		redir_file;
+	int		search_r_file;
 
 	search_cmd = 1;
-	redir_file = 0;
-	ptr_node = ptr_cmd->first_token;
-	while (ptr_node != NULL)
+	search_r_file = 0;
+	token = cmd->first_token;
+	while (token != NULL)
 	{
-		if (search_cmd == 1 && redir_file == 0 && !is_operator_type(ptr_node))
+		if (search_cmd == 1 && search_r_file == 0 && !is_operator_type(token))
 		{
-			ptr_node->type = assign_cmd_type(ptr_node->text);
+			token->type = assign_cmd_type(token->text);
 			search_cmd = 0;
 		}
-		else if (is_redirection_type(ptr_node))
-			redir_file = 1;
-		else if (redir_file == 1 && !is_operator_type(ptr_node))
+		else if (is_redirection_type(token))
+			search_r_file = 1;
+		else if (search_r_file == 1 && !is_operator_type(token))
 		{
-			ptr_node->type = assign_file_type(ptr_node->prev->type);
-			redir_file = 0;
+			token->type = assign_file_type(token->prev->type);
+			search_r_file = 0;
 		}
-		else if (search_cmd == 0 && !is_operator_type(ptr_node))
-			ptr_node->type = ARG;
-		else if (is_pipe_type(ptr_node))
+		else if (search_cmd == 0 && !is_operator_type(token))
+			token->type = ARG;
+		else if (is_pipe_type(token))
 			search_cmd = 1;
-		ptr_node = ptr_node->next;
+		token = token->next;
 	}
 }
 
-void	assign_types_to_tokens(t_cmd *ptr_cmd)
+void	assign_types_to_tokens(t_cmd *cmd)
 {
-	assign_operator_types(ptr_cmd);
-	assign_cmds_and_args(ptr_cmd);
+	assign_operator_types(cmd);
+	assign_cmds_and_args(cmd);
 }
