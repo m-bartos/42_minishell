baf4aba1d68ee1a2e72777adc412d0f87567e30e
m-bartos <m.bartos96@seznam.cz>
2024-03-12 12:53:53 +0100

    Clean: helpers.c

--- utils/helpers.c
+++ utils/helpers.c
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 12:49:32 by aldokezer         #+#    #+#             */
-/*   Updated: 2024/03/08 16:27:12 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/03/12 12:53:29 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 void	ft_print_cmd(t_cmd *cmd)
 {
-	t_token	*ptr_token;
+	t_token	*token;
 
-	ptr_token = cmd->first_token;
-	while (ptr_token != NULL)
+	token = cmd->first_token;
+	while (token != NULL)
 	{
-		ft_printf(" %s", ptr_token->text);
-		ptr_token = ptr_token->next;
+		ft_printf(" %s", token->text);
+		token = token->next;
 	}
 	ft_putstr_fd("\n", 1);
 }
