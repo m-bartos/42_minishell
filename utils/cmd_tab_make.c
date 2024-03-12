cc1f9245e91e9409970f81320d4b51a52b98c53f
m-bartos <m.bartos96@seznam.cz>
2024-03-12 12:46:53 +0100

    Clean: cmd_tab_make.c

--- utils/cmd_tab_make.c
+++ utils/cmd_tab_make.c
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/10 10:22:34 by mbartos           #+#    #+#             */
-/*   Updated: 2024/03/10 10:23:04 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/03/12 12:46:44 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,17 @@
 void	make_cmd_tab(t_cmd_tab *cmd_tab, t_cmd *parsed_line)
 {
 	t_token	*token;
-	t_token	*temp;
+	t_token	*temp_token;
 
 	token = parsed_line->first_token;
 	ft_push_rotate_cmd(cmd_tab);
-	while(token != NULL)
+	while (token != NULL)
 	{
-		temp = token->next;
+		temp_token = token->next;
 		if (is_pipe_type(token->prev))
 			ft_push_rotate_cmd(cmd_tab);
 		ft_move_token(cmd_tab->last_cmd, token);
-		token = temp;
+		token = temp_token;
 	}
 	ft_init_cmd_struct(parsed_line);
 }
