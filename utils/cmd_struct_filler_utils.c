7a6ffe7ad87339dce5e15415b64916d4cb574869
m-bartos <m.bartos96@seznam.cz>
2024-03-12 12:37:48 +0100

    Clean: cmd_struct_filler_utils.c

--- utils/cmd_struct_filler_utils.c
+++ utils/cmd_struct_filler_utils.c
@@ -6,62 +6,58 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 14:18:05 by mbartos           #+#    #+#             */
-/*   Updated: 2024/03/08 21:54:43 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/03/12 12:37:20 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-int	is_pipe_type(t_token *ptr_token)
+int	is_pipe_type(t_token *token)
 {
-	int	type;
-
-	if (ptr_token == NULL)
+	if (token == NULL)
 		return (0);
-	type = ptr_token->type;
-	if (type == PIPE)
+	if (token->type == PIPE)
 		return (1);
 	return (0);
 }
 
-int	is_redirection_type(t_token *ptr_token)
+int	is_redirection_type(t_token *token)
 {
 	int	type;
 
-	type = ptr_token->type;
-	if (type == R_IN || type == HERE_DOC
-		|| type == R_OUT || type == R_OUT_APP)
+	type = token->type;
+	if (type == R_IN || type == HERE_DOC || type == R_OUT || type == R_OUT_APP)
 		return (1);
 	return (0);
 }
 
-int	is_operator_type(t_token *ptr_token)
+int	is_operator_type(t_token *token)
 {
-	if (is_pipe_type(ptr_token) || is_redirection_type(ptr_token))
+	if (is_pipe_type(token) || is_redirection_type(token))
 		return (1);
 	return (0);
 }
 
-int	is_in_single_quotes(t_token *ptr_token)
+int	is_in_single_quotes(t_token *token)
 {
-	char	*token;
-	int		len;
+	char	*text;
+	int		length;
 
-	token = ptr_token->text;
-	len = ft_strlen(token);
-	if (token[0] == '\'' && token[len - 1] == '\'')
+	text = token->text;
+	length = ft_strlen(text);
+	if (text[0] == '\'' && text[length - 1] == '\'')
 		return (1);
 	return (0);
 }
 
-int	is_in_double_quotes(t_token *ptr_token)
+int	is_in_double_quotes(t_token *token)
 {
-	char	*token;
-	int		len;
+	char	*text;
+	int		length;
 
-	token = ptr_token->text;
-	len = ft_strlen(token);
-	if (token[0] == '"' && token[len - 1] == '"')
+	text = token->text;
+	length = ft_strlen(text);
+	if (text[0] == '\"' && text[length - 1] == '\"')
 		return (1);
 	return (0);
 }
