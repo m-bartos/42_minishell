c63286f7900ba933373b6a3526de56d5cf4ba342
m-bartos <m.bartos96@seznam.cz>
2024-03-14 15:36:32 +0100

    Implement: press of CTRL+D

--- utils/exit_free.c
+++ utils/exit_free.c
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/10 10:24:15 by mbartos           #+#    #+#             */
-/*   Updated: 2024/03/14 15:18:32 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/03/14 15:35:19 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	check_exit(char *line)
 {
-	if (ft_strncmp(line, "exit", 5) == 0)
+	if (line == NULL || ft_strncmp(line, "exit", 5) == 0)
 	{
 		printf("exit\n");
 		free(line);
