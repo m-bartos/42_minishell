37119cda0b43ac5d999001ce4f2307090884694d
m-bartos <m.bartos96@seznam.cz>
2024-03-16 20:42:07 +0100

    Fix: output error message to STDERR

--- utils/cmd_struct_errors.c
+++ utils/cmd_struct_errors.c
@@ -6,11 +6,21 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 11:23:21 by mbartos           #+#    #+#             */
-/*   Updated: 2024/03/12 12:33:04 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/03/16 20:41:36 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+void	exit_redirection_error(t_cmd *cmd, char *text)
+{
+	ft_putstr_fd("minishell: syntax error near unexpected token '", 2);
+	ft_putstr_fd(text, STDERR);
+	ft_putstr_fd("'\n", STDERR);
+	ft_delete_cmd(cmd);
+	printf(RESET);
+	exit(2);
+}
 
 void	check_redirection_errors(t_cmd *cmd)
 {
@@ -20,22 +30,13 @@ void	check_redirection_errors(t_cmd *cmd)
 	while (token != NULL)
 	{
 		if (((is_operator_type(token) || is_pipe_type(token))
-			&& is_redirection_type(token->prev)) ||
-			(is_pipe_type(token) && is_pipe_type(token->prev)))
+				&& is_redirection_type(token->prev))
+			|| (is_pipe_type(token) && is_pipe_type(token->prev)))
 		{
-			printf("minishell: syntax error near unexpected token '%s'\n", 
-				token->text);
-			ft_delete_cmd(cmd);
-			printf(RESET);
-			exit(0);
+			exit_redirection_error(cmd, token->text);
 		}
 		token = token->next;
 	}
 	if (is_redirection_type(cmd->last_token))
-	{
-		printf("minishell: syntax error near unexpected token 'newline'\n");
-		ft_delete_cmd(cmd);
-		printf(RESET);
-		exit(0);
-	}
+		exit_redirection_error(cmd, "newline");
 }
