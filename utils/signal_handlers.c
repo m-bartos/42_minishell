61bb67e11d070dea8cea3fa1ff546a721a2f6832
O.R <orezek@seznam.cz>
2024-03-17 10:26:44 +0100

    Update: rename signals file and add comments to functions

--- utils/signal_handlers.c
+++ utils/signal_handlers.c
@@ -6,11 +6,26 @@
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 10:49:43 by aldokezer         #+#    #+#             */
-/*   Updated: 2024/03/14 17:03:53 by aldokezer        ###   ########.fr       */
+/*   Updated: 2024/03/17 10:25:42 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+/**
+ * @brief Handles the SIGINT signal (Ctrl+C).
+ *
+ * This function is called when a SIGINT signal is received, typically through
+ * a Ctrl+C action in the terminal. It can be used to load a new prompt or
+ * perform clean-up actions. The received signal is not passed to execve,
+ * but inbuilt functions will inherit this behavior, and they should handle
+ * the signal accordingly.
+ *
+ * @note This function may exit the program or perform specific actions
+ *       like clearing input or displaying a new prompt.
+ *
+ * @param signal The signal number received (expected to be SIGINT).
+ */
 
 void	ft_ctrl_c_sig(int signal)
 {
@@ -21,8 +36,20 @@ void	ft_ctrl_c_sig(int signal)
 	//exit(1);
 }
 
-// no need for this function, it is ignored in the signal function
-// by providing macro instead of a pointer to a function
+/**
+ * @brief Handles the SIGQUIT signal (Ctrl+\).
+ *
+ * This function is intended to respond to a SIGQUIT signal, triggered by
+ * pressing Ctrl+\ in the terminal. It's designed to be ignored or to perform
+ * specific actions, like printing a message, as part of a custom signal
+ * handling strategy.
+ *
+ * @note By default, this behavior is set to be ignored in the program's
+ *       signal handling setup.
+ *
+ * @param signal The signal number received (expected to be SIGQUIT).
+ */
+
 void	ft_ctrl_slash_sig(int signal)
 {
 	ft_putstr_fd("\nYou pressed ctrl + \\ and the signal was: ", 1);
