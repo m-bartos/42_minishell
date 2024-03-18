d93200598453e58e4d949fa9d5526429422f81eb
m-bartos <m.bartos96@seznam.cz>
2024-03-18 17:08:33 +0100

    Update: doc in cmd_struct_filler.c

--- utils/cmd_struct_filler.c
+++ utils/cmd_struct_filler.c
@@ -6,12 +6,22 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 13:04:25 by mbartos           #+#    #+#             */
-/*   Updated: 2024/03/15 08:22:25 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/03/18 17:08:12 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+/**
+ * @brief Fills a command structure with tokens from an array of strings.
+ *
+ * This function populates the specified command structure with tokens obtained
+ * from the provided array of strings. It iterates through the array of tokens,
+ * adding each token to the command structure.
+ *
+ * @param cmd The command structure to fill with tokens.
+ * @param arr_of_tokens The array of strings (tokens) to add to the command.
+ */
 void	fill_cmd_tab(t_cmd *cmd, char **arr_of_tokens)
 {
 	size_t	i;
