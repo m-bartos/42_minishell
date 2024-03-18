8fd2d9ba5bed3863bb1797428f178b1e242de26b
m-bartos <m.bartos96@seznam.cz>
2024-03-18 16:44:44 +0100

    Update: doc in cmd_tab_make.c

--- utils/cmd_tab_make.c
+++ utils/cmd_tab_make.c
@@ -6,12 +6,24 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/10 10:22:34 by mbartos           #+#    #+#             */
-/*   Updated: 2024/03/18 09:49:54 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/03/18 16:43:57 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+/**
+ * @brief Constructs a command table from a command.
+ * 
+ * This function constructs a command table from a given command by iterating
+ * through its tokens and organizing them into separate commands within the
+ * command table. It ensures that each command is correctly positioned within
+ * the command table, taking into account pipe operators as separators between
+ * commands.
+ * 
+ * @param cmd_tab Pointer to the command table to populate.
+ * @param cmd Pointer to the command containing tokens to be organized.
+ */
 void	make_cmd_tab_from_cmd(t_cmd_tab *cmd_tab, t_cmd *cmd)
 {
 	t_token	*token;
