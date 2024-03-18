d4a6390594c998d161b1a56ac86268a4c0014447
m-bartos <m.bartos96@seznam.cz>
2024-03-18 17:06:51 +0100

    Update: doc in cmd_struct_remove_quotes_utils.c

--- utils/cmd_struct_remove_quotes_utils.c
+++ utils/cmd_struct_remove_quotes_utils.c
@@ -6,12 +6,33 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 16:52:08 by mbartos           #+#    #+#             */
-/*   Updated: 2024/03/18 16:52:20 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/03/18 17:06:22 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/**
+ * @file cmd_struct_remove_quotes_utils.c
+ * @brief Utility functions for removing quotes from token text within command
+ * structures.
+ *
+ * This file contains utility functions used in the process of removing quotes
+ * from token text within command structures. It includes functions for parsing
+ * token text, removing quotes encaptulating words, and obtaining substrings
+ * from words.
+ */
+
 #include "../minishell.h"
 
+/**
+ * @brief Removes quotes encaptulating words from an array of strings.
+ * 
+ * This function removes quotes ('"' or '\'') encaptulating words within each
+ * string of the specified array. It iterates through the array and trims quotes
+ * from the beginning and end of each string if present.
+ * 
+ * @param arr_of_str The array of strings to process.
+ * @return The updated array of strings with quotes removed from each word.
+ */
 char	**remove_quotes_encaptulates_words(char **arr_of_str)
 {
 	size_t	i;
@@ -37,6 +58,18 @@ char	**remove_quotes_encaptulates_words(char **arr_of_str)
 	return (arr_of_str);
 }
 
+/**
+ * @brief Obtains a substring from a word in the given text.
+ * 
+ * This function extracts a substring from a word in the specified text.
+ * It identifies the end of the word based on the specified letter to find 
+ * (quote character). It then calculates the size of the substring and obtains
+ * it.
+ * 
+ * @param str The text containing the word.
+ * @param i Pointer to the index indicating the position within the text.
+ * @return The substring extracted from the word.
+ */
 char	*get_substr_from_word(char const *str, size_t *i)
 {
 	char	letter_to_find;
@@ -61,7 +94,13 @@ char	*get_substr_from_word(char const *str, size_t *i)
 	return (new_text);
 }
 
-int	count_substrs_in_word(char const *text)
+/**
+ * @brief Counts the number of substrings in a word within the text.
+ * 
+ * @param text The text to process.
+ * @return The number of substrings in the word.
+ */
+int	count_substrs_in_word(char const *word)
 {
 	char	*temp_str;
 	size_t	i;
@@ -69,27 +108,37 @@ int	count_substrs_in_word(char const *text)
 
 	i = 0;
 	size = 0;
-	while (text[i])
+	while (word[i])
 	{
-		temp_str = get_substr_from_word(&text[i], &i);
+		temp_str = get_substr_from_word(&word[i], &i);
 		free(temp_str);
 		size++;
 	}
 	return (size);
 }
 
-char	**parse_token_text(char *text)
+/**
+ * @brief Parses token text into an array of strings.
+ * 
+ * This function parses the specified token text into an array of strings.
+ * It counts the number of substrings in the text, allocates memory for
+ * the array, and obtains substrings from token text.
+ * 
+ * @param text The token text to parse.
+ * @return The array of strings containing parsed words from the token text.
+ */
+char	**parse_token_text(char *word)
 {
 	char	**arr_of_strs;
 	size_t	i;
 	size_t	j;
 	int		size;
 
-	size = count_substrs_in_word(text);
+	size = count_substrs_in_word(word);
 	arr_of_strs = ft_init_array(size);
 	i = 0;
 	j = 0;
-	while (text[i])
-		arr_of_strs[j++] = get_substr_from_word(&text[i], &i);
+	while (word[i])
+		arr_of_strs[j++] = get_substr_from_word(&word[i], &i);
 	return (arr_of_strs);
 }
