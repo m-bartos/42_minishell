839a3667dafdfe865aee8a3f24aed09439f991d3
m-bartos <m.bartos96@seznam.cz>
2024-03-18 16:57:01 +0100

    Update: doc in cmd_struct_remove_quotes.c

--- utils/cmd_struct_remove_quotes.c
+++ utils/cmd_struct_remove_quotes.c
@@ -6,12 +6,31 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 14:44:37 by mbartos           #+#    #+#             */
-/*   Updated: 2024/03/18 16:52:06 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/03/18 16:56:32 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/**
+ * @file cmd_struct_remove_quotes.c
+ * @brief Functions for removing quotes from tokens within command structures.
+ *
+ * This file contains functions related to removing quotes from tokens within
+ * command structures. It includes functionality for parsing token text, 
+ * removing quotes surrounding words, and updating token text accordingly to
+ * eliminate quotes.
+ */
+
 #include "../minishell.h"
 
+/**
+ * @brief Removes quotes from the text of a token.
+ * 
+ * This function removes quotes from the text of the specified token. It parses
+ * the token text, removes quotes surrounding words, and updates the token text
+ * accordingly to eliminate the quotes.
+ * 
+ * @param token The token to remove quotes from.
+ */
 void	remove_quotes_in_token(t_token *token)
 {
 	char	*new_token;
@@ -27,6 +46,15 @@ void	remove_quotes_in_token(t_token *token)
 	free(temp_token_text);
 }
 
+/**
+ * @brief Removes quotes from tokens within a command.
+ * 
+ * This function removes quotes from tokens within the specified command. It
+ * iterates through the tokens of the command and calls the 
+ * 'remove_quotes_in_token' function to remove quotes from non-operator tokens.
+ * 
+ * @param cmd The command to remove quotes from its tokens.
+ */
 void	remove_quotes_in_cmd_tokens(t_cmd *cmd)
 {
 	t_token	*token;
