54e5fe40fe46cfb60fc7a18c906b48d14411db0e
m-bartos <m.bartos96@seznam.cz>
2024-03-18 13:02:10 +0100

    Update: add doc to parser.c

--- utils/parser.c
+++ utils/parser.c
@@ -6,12 +6,21 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 14:20:49 by mbartos           #+#    #+#             */
-/*   Updated: 2024/03/18 12:55:50 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/03/18 13:01:54 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+/**
+ * @brief Handles the case when the last token in the command is a pipe.
+ *
+ * Reads input from the user until a non-pipe token is encountered,
+ * splits the input into tokens, and adds them to a new command structure.
+ * Joins the new command structure with the original command structure.
+ *
+ * @param cmd The command structure to handle.
+ */
 void	handle_if_last_token_is_pipe(t_cmd *cmd)
 {
 	char	*line_heredoc;
@@ -29,7 +38,13 @@ void	handle_if_last_token_is_pipe(t_cmd *cmd)
 	}
 }
 
-void	parse_from_arr_of_tokens_to_one_cmd(t_cmd *cmd, char **arr_of_tokens)
+/**
+ * @brief Parses an array of tokens into a single command structure.
+ * 
+ * @param cmd The command structure to fill with parsed data.
+ * @param arr_of_tokens The array of tokens to parse.
+ */
+void parse_from_arr_of_tokens_to_one_cmd(t_cmd *cmd, char **arr_of_tokens)
 {
 	fill_cmd_tab(cmd, arr_of_tokens);
 	free(arr_of_tokens);
@@ -42,7 +57,7 @@ void	parse_from_arr_of_tokens_to_one_cmd(t_cmd *cmd, char **arr_of_tokens)
 }
 
 /**
- * Parses the given line and populates the command table.
+ * @brief Parses the given line and populates the command table.
  *
  * @param cmd_tab The command table to populate.
  * @param line The line to parse.
