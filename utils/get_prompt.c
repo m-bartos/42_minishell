78f475376b7f5b7421a7c8a5fc8e0bd26f0772f2
m-bartos <m.bartos96@seznam.cz>
2024-03-25 10:53:43 +0100

    Update: get_prompt*.c files

--- utils/get_prompt.c
+++ utils/get_prompt.c
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 15:24:00 by mbartos           #+#    #+#             */
-/*   Updated: 2024/03/21 14:45:30 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/03/25 10:31:44 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,19 +58,26 @@ char	*get_relative_path(t_env_list *env_list)
 	char	*path_start;
 
 	absolute_path = ft_get_env(env_list, "PWD");
+	if (absolute_path == NULL)
+		absolute_path = getcwd(NULL, 0);
 	home = ft_get_env(env_list, "HOME");
-	if (ft_strlen(absolute_path) >= ft_strlen(home))
-	{
-		relative_path = absolute_path + ft_strlen(home);
-		path_start = ft_strdup_e(":~");
-	}
+	if (home == NULL)
+		relative_path = ft_strdup(absolute_path);
 	else
 	{
-		relative_path = absolute_path;
-		path_start = ft_strdup_e(":");
+		if (ft_strlen(absolute_path) >= ft_strlen(home))
+		{
+			relative_path = absolute_path + ft_strlen(home);
+			path_start = ft_strdup_e(":~");
+		}
+		else
+		{
+			relative_path = absolute_path;
+			path_start = ft_strdup_e(":");
+		}
+		relative_path = ft_strjoin_e(path_start, relative_path);
+		free(path_start);
 	}
-	relative_path = ft_strjoin_e(path_start, relative_path);
-	free(path_start);
 	free(absolute_path);
 	free(home);
 	return (relative_path);
--- utils/get_prompt_errors.c
+++ utils/get_prompt_errors.c
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 13:57:57 by mbartos           #+#    #+#             */
-/*   Updated: 2024/03/21 14:28:53 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/03/25 10:32:13 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,7 +56,7 @@ int	check_prompt_vars(t_env_list *env_list)
 		is_error = TRUE;
 	if (is_var_in_env_list(env_list, "PWD") == FALSE)
 		is_error = TRUE;
-	if (is_var_in_env_list(env_list, "HOME") == FALSE)
-		is_error = TRUE;
+	// if (is_var_in_env_list(env_list, "HOME") == FALSE)
+	// 	is_error = TRUE;
 	return (is_error);
 }
