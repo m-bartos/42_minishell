375ff3d45dc9d557c28de6e98daa54ae718e9c4e
m-bartos <m.bartos96@seznam.cz>
2024-04-21 13:18:02 +0200

    Clean: main.c

--- main.c
+++ main.c
@@ -6,19 +6,13 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 14:09:57 by aldokezer         #+#    #+#             */
-/*   Updated: 2024/04/21 13:10:53 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/04/21 13:17:41 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "./minishell.h"
 
-
-// valgrind -s --leak-check=full --show-reachable=yes --error-limit=no --suppressions=minishell.supp --trace-children=yes --track-fds=yes ./minishell
-// echo "jojo" 'nene' > outfile.txt | < infile.txt echo "jojo" 'nene' aha "$USER" '$USER' $USER >> test.out
-
-
-int	main (int argc, char *argv[], char *envp[])
+int	main(int argc, char **argv, char **envp)
 {
 	char		*prompt;
 	char		*line;
@@ -32,12 +26,8 @@ int	main (int argc, char *argv[], char *envp[])
 	ft_init_minidata(&minidata, &cmd_tab, envp);
 	exit_minishell(&minidata, 0);
 	clean_minishell(&minidata);
-	// Get PID to see process information
-	// ft_printf("%d\n", getpid());
 	while (1)
 	{
-		// ft_putstr_fd(BLUE, 1);
-		// ft_putstr_fd(BLUE, 2);
 		if (g_sigint_received)
 			g_sigint_received = 0;
 		prompt = get_prompt(&minidata);
@@ -47,14 +37,12 @@ int	main (int argc, char *argv[], char *envp[])
 			ft_exit_minishell(NULL, NULL, NULL);
 		if (is_empty_line(line))
 			continue ;
-		// check_exit(line);
 		if (*line)
 			add_history(line);
 		if (check_unclosed_quotes(line))
 			continue ;
 		if (parser(&cmd_tab, line, &minidata) == -1)
 			continue ;
-		// print_cmd_tab(&cmd_tab); // just to show cmd_tab
 		if (ft_pre_exec(&cmd_tab, &minidata))
 		{
 			clean_minishell(NULL);
