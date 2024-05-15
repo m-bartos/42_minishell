2277c8d9decbe38363fa8b8445ef4a7bc90e2f62
m-bartos <m.bartos96@seznam.cz>
2024-05-15 09:34:32 +0200

    Clean: add header doc to signal_handlers.c

--- src/utils/signal_handlers.c
+++ src/utils/signal_handlers.c
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 10:49:43 by aldokezer         #+#    #+#             */
-/*   Updated: 2024/05/15 09:27:45 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/05/15 09:34:13 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,16 @@
 
 volatile sig_atomic_t	g_sigint_received = 0;
 
+/**
+ * @brief Signal handler for SIGINT (Ctrl+C).
+ * 
+ * This function handles the SIGINT signal, which is typically generated 
+ * by pressing Ctrl+C. It writes a newline character to the standard output,
+ * resets the state of the readline library, and updates a global variable
+ * to indicate the signal was received.
+ * 
+ * @param sig The signal number (unused in this function).
+ */
 void	sigint_handler(int sig)
 {
 	(void)sig;
@@ -24,6 +34,14 @@ void	sigint_handler(int sig)
 	g_sigint_received = 130;
 }
 
+/**
+ * @brief Sets up signal handling for SIGINT, SIGQUIT and Ctrl+C
+ * 
+ * This function configures the signal handlers for SIGINT (Ctrl+C) 
+ * and SIGQUIT (Ctrl+\). SIGINT is handled by the `sigint_handler` function, 
+ * and SIGQUIT is ignored. Additionally, it disables the output of the Ctrl+C 
+ * sequence in the terminal.
+ */
 void	setup_signal_handling(void)
 {
 	struct sigaction	sa;
@@ -37,6 +55,13 @@ void	setup_signal_handling(void)
 	disable_ctrl_c_output();
 }
 
+/**
+ * @brief Disables the display of Ctrl+C output in the terminal.
+ * 
+ * This function modifies the terminal settings to disable the echoing of Ctrl+C
+ * (SIGINT) control character. It adjusts the terminal's local flags to turn off
+ * the ECHOCTL option.
+ */
 void	disable_ctrl_c_output(void)
 {
 	struct termios	term;
@@ -46,6 +71,15 @@ void	disable_ctrl_c_output(void)
 	tcsetattr(STDIN_FILENO, TCSANOW, &term);
 }
 
+/**
+ * @brief Signal handler for SIGINT during heredoc input.
+ * 
+ * This function handles SIGINT signals received during heredoc input. It writes
+ * a newline character to the standard output, cleans up command resources, 
+ * and exits the minishell with a status code of 130.
+ * 
+ * @param sig The signal number (unused in this function).
+ */
 void	handle_sigint_heredoc(int sig)
 {
 	(void)sig;
@@ -54,6 +88,15 @@ void	handle_sigint_heredoc(int sig)
 	exit_minishell(NULL, 130);
 }
 
+/**
+ * @brief Signal handler for SIGINT in the heredoc parent process.
+ * 
+ * This function handles SIGINT signals in the parent process during heredoc
+ * input. It sets a global variable to indicate that the SIGINT signal
+ * was received, with a status code of 130.
+ * 
+ * @param sig The signal number (unused in this function).
+ */
 void	handle_sigint_heredoc_parent(int sig)
 {
 	(void)sig;
